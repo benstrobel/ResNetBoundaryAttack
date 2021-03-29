01b0fbd45b1e7187b17996bc0e06555cf538cf59
Ben <benedikt.strobel@tum.de>
2021-03-29 23:36:57 +0200

    Added Multiple Instance Execution

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import os
 import random
 import numpy as np
 import datetime
+import sys
 
 
 def render_as_image(a):
@@ -27,6 +28,35 @@ def save_image(name, date, img):
 
 def save_figure(name, date):
     pyplot.savefig(os.getcwd() + "\\Result\\" + date.strftime("%Y%m%d%H%M%S") + "\\" + name + ".png")
+
+def getScore(instance):
+    if instance.lastStep is None or instance.lastStep == 0:
+        return instance.boundaryAttack.getCurrentDist()
+    else:
+        return instance.boundaryAttack.getCurrentDist() * pow(instance.lastStep, 2)
+
+def getInstanceWithLowestDist(instanceList):
+    lowestCalculatedStep = sys.maxsize
+    for x in instanceList:
+        if x.lastStep < lowestCalculatedStep:
+            lowestCalculatedStep = x.lastStep
+
+    if lowestCalculatedStep == sys.maxsize or lowestCalculatedStep is None or lowestCalculatedStep == 0:
+        return instanceList[0]
+
+    lowestCalculatedStep = lowestCalculatedStep - 1
+
+    currentLowest = instanceList[0]
+    currentLowestScore = instanceList[0].distance_list[lowestCalculatedStep]
+
+    if len(instanceList) > 1:
+        for x in instanceList:
+            currentScore = x.distance_list[lowestCalculatedStep]
+            if currentScore < currentLowestScore:
+                currentLowest = x
+                currentLowestScore = currentScore
+    return currentLowest
+
 
 # Setting Mean And Std Array
 mean_r = mx.nd.full((1, 224, 224), 0.485)
@@ -73,7 +103,7 @@ class AttackInstance:
         self.distance_list = []
         self.alpha_list = []
         self.beta_list = []
-        self.lastStep = None
+        self.lastStep = 0
         self.date = datetime.datetime.now()
         os.mkdir(os.getcwd()+"\\Result\\" + self.date.strftime("%Y%m%d%H%M%S"))
         save_image("start", self.date, img)
@@ -81,7 +111,7 @@ class AttackInstance:
 
     def step(self):
         self.boundaryAttack.step()
-        if self.lastStep == None or self.lastStep < self.boundaryAttack.getCurrentStep():
+        if self.lastStep == 0 or self.lastStep is None or self.lastStep < self.boundaryAttack.getCurrentStep():
             self.lastStep = self.boundaryAttack.getCurrentStep()
             self.distance_list.append(self.boundaryAttack.getCurrentDist())
             self.alpha_list.append(self.boundaryAttack.getCurrentAlpha())
@@ -120,37 +150,40 @@ class AttackInstance:
 sealionImg = image.imread(sealionPath + sealionList[random.randrange(0, len(sealionList))])
 
 forkLiftImgList = []
-intanceList = []
+instanceList = []
 instances = 4
 
 for x in range(instances):
     forkLiftImgList.append(image.imread(forkLiftPath + forkLiftList[random.randrange(0, len(forkLiftList))]))
 
 for x in range(instances):
-    intanceList.append(AttackInstance(sealionImg, None))
+    instanceList.append(AttackInstance(sealionImg, forkLiftImgList[x]))
 
-for currentInstance in intanceList:
-    while currentInstance.boundaryAttack.getCurrentStep() < 1000 and below_convergence_limit_counter < 5:
-        currentInstance.step()
-    currentInstance.finish()
+currentInstance = getInstanceWithLowestDist(instanceList)
 
-for x in intanceList:
-    pyplot.plot(x.distance_list)
-pyplot.ylabel("L2-Distance")
+total_steps = 0
+stepList = []
+
+while total_steps < 1000 and below_convergence_limit_counter < 5:
+    cur = currentInstance
+    if total_steps % (25 * instances) == 0:
+        print("Performing " + str(2*instances) + " Steps for every Instance")
+        for x in instanceList:
+            for i in range(2*instances):
+                curStep = x.lastStep
+                while curStep == x.lastStep:
+                    x.step()
+                    stepList.append(instanceList.index(x))
+        currentInstance = getInstanceWithLowestDist(instanceList)
+    if cur != currentInstance:
+        print("Swapped Instance " + str(instanceList.index(cur)) + " -> " + str(instanceList.index(currentInstance)))
+    currentInstance.step()
+    stepList.append(instanceList.index(currentInstance))
+    total_steps = total_steps + 1
+currentInstance.finish()
+
+pyplot.plot(stepList)
+pyplot.ylabel("Index der Aktuellen Instanz")
 pyplot.xlabel("Step")
-save_figure("distance-all", x.date)
-pyplot.show()
-
-for x in intanceList:
-    pyplot.plot(x.alpha_list)
-pyplot.ylabel("Alpha")
-pyplot.xlabel("Step")
-save_figure("alpha-all", x.date)
-pyplot.show()
-
-for x in intanceList:
-    pyplot.plot(x.beta_list)
-pyplot.ylabel("Beta")
-pyplot.xlabel("Step")
-save_figure("beta-all", x.date)
+save_figure("index", currentInstance.date)
 pyplot.show()
